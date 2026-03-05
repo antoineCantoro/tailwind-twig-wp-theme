6c815eeffc504eb952d6c22c50d2927c833e9070
Antoine <antoine.cantoro@gmail.com>
2026-03-05 14:58:09 +0100

    FIX: remove vendor folder

--- functions.php
+++ functions.php
@@ -141,13 +141,13 @@ function test_scripts() {
 	wp_enqueue_style( 'test-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'test-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'test-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	// wp_enqueue_script( 'test-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'test_scripts' );
+add_action('wp_enqueue_scripts', 'test_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -241,8 +241,25 @@ function vite_is_dev_server_running(): bool {
  */
 function wps_deregister_styles() {
 	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'global-styles-inline-css' );
 	wp_dequeue_style( 'global-styles' );
 	wp_dequeue_style( 'wp-emoji-styles' );
+	wp_dequeue_style( 'wp-emoji-release' );
 	wp_dequeue_style( 'classic-theme-styles' );
 }
+
+/**
+ * Deregister the js
+ */
+function wps_deregister_scripts() {
+
+	wp_dequeue_script( 'wp-emoji-styles' );
+	wp_dequeue_script( 'wp-emoji-release' );
+}
 add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
+add_action( 'wp_print_scripts', 'wps_deregister_scripts', 100 );
+add_filter( 'should_load_separate_core_block_assets', '__return_false', 99 );
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_enqueue_scripts', 'wp_enqueue_emoji_styles' );
+remove_action( 'wp_head', 'wp_img_auto_sizes_contain_inline_css' );
